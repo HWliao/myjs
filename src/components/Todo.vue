02fb6da9a53439b7b443da3b866330ebede147d9
HWliao <499606788@qq.com>
2017-03-21 18:20:51 +0800

    vue todo hello done

---
+++ src/components/Todo.vue
@@ -0,0 +1,11 @@
+<style>
+
+</style>
+<template>
+  <h1>Hello todo !</h1>
+</template>
+<script>
+  export default {
+    name: 'todo',
+  };
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Hello from '../components/Hello';
+import Todo from '../components/Todo';
 
 Vue.use(Router);
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    }, {
+      path: '/todo',
+      name: 'todo',
+      component: Todo,
     },
   ],
 });
