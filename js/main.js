6eeecabeb61d497763deaf041b1a064de9dcd2fb
HWliao <499606788@qq.com>
2017-09-18 23:12:41 +0800

    加入MSelect组件

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 (function () {
   'use strict';
+  // 添加自定义组件
+  Vue.component('mSelect', MSelect);
   // 路由
   Vue.use(VueRouter);
 
@@ -10,11 +12,11 @@
         dark: false,
         drawer: true,
         items: [
-          { icon: 'home', text: '首页' },
+          {icon: 'home', text: '首页'},
         ],
         items2: [
-          { id: 1, name: 'seal', icon: 'mail', href: '/gz/seal', text: '印章信息' },
-          { id: 2, name: 'file', icon: 'folder', href: '/gz/file', text: '文件资料' }
+          {id: 1, name: 'seal', icon: 'mail', href: '/gz/seal', text: '印章信息'},
+          {id: 2, name: 'file', icon: 'folder', href: '/gz/file', text: '文件资料'}
         ]
       };
     },
