4b31afdd5ec2332c8b7faa9baf6f1fccc777051a
HWliao <499606788@qq.com>
2017-09-25 22:21:54 +0800

    对iview一瞬间失去兴趣了

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'app',
+    data() {
+      return {};
+    },
+    computed: {},
+    methods: {},
+  };
+</script>
+
+<style scoped>
+
+  .v-clack {
+    display: none;
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import './theme/style.less';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+});
