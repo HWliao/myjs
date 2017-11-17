58538fc971644c07712f4064924b4bca7f0cddfc
HWliao <499606788@qq.com>
2017-11-17 23:30:18 +0800

    ng-zorro 简直是一个玩具 连最基本的主题的修改都不支持,还要组件改源码 暂停学习

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
 
-import { HeroService } from './hero.service';
-
-import { AppRoutingModule } from './app-routing.module';
-import { HeroSearchComponent } from './hero-search.component';
-
-// Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-    DashboardComponent,
-    HeroSearchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService)
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [HeroService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
