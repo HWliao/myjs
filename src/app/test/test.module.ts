48150bcbcfc858c4aa4bcdccd2794f4aa5158bbb
1149023208@qq.com <zxxxxx11>
2017-11-19 19:24:32 +0800

    创建模块

---
+++ src/app/test/test.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TestComponent } from './test.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ TestComponent ],
+  exports: [ TestComponent ]
+})
+export class TestModule { }
