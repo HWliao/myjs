6e70e894cbb650726be4d57ef8170d5733a14d76
HWliao <499606788@qq.com>
2017-11-26 21:20:13 +0800

    config dev done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello world!');
+const name = '廖红卫';
+const html = `<h1>i'm ${name},hello!</h1>`;
+const div = document.createElement('div');
+div.innerHTML = html;
+document.body.appendChild(div);
+
