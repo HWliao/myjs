32e6a76592e35eaa8dc7ffc90111829d7bcb5558
HWliao <499606788@qq.com>
2017-12-27 00:04:15 +0800

    完成ng2 手动引导启动整个组件树 一个platform,多个application的启动控制

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,19 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  ngDoBootstrap() {
+    // NgModule bootstap属性为空
+    // 如果NgModule bootstap 没有设置 ngDoBootstrap 在引导启动时会被调用
+
+    // 什么都不用做
+    // NgModule bootstap 不设置,ngDoBootstrap 毁掉钩子给一个空函数
+    // 导致Module被引导启动的时候,只做一些初始化动作,并不会渲染整个应用
+
+    // NgModule entryComponents 指定入口组件,在Module初始化的时候会产生对应的ComponentFactory
+    // 当Ngmodule被引导初始化后,可以通过NgModuleRef获取到ComponentFactory和ApplicationRef
+    // 并通过ApplicationRef.bootstrap() 手动控制整个应用的UI渲染
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,53 @@
-import { enableProdMode } from '@angular/core';
+import {
+  ApplicationRef, ComponentFactory, enableProdMode, NgModuleRef, PlatformRef
+} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
+let platform: PlatformRef;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+const button = document.getElementById('test');
+const button2 = document.getElementById('test2');
+
+
+button.addEventListener('click', () => {
+  platform = platformBrowserDynamic();
+
+  // 应用1 初始化
+  const div1 = document.createElement('div');
+  document.body.appendChild(div1);
+  platform.bootstrapModule(AppModule)
+    .then((moduleRef: NgModuleRef<AppModule>) => {
+      // 在这里达成一个platform,多个application启动控制
+
+      // 获取应用应用
+      const applicationRef = moduleRef.injector.get(ApplicationRef);
+      // 获取入口组件
+      const componentFactory: ComponentFactory<AppComponent> = moduleRef.componentFactoryResolver.resolveComponentFactory(AppComponent);
+      // 引导整个组件树的初始化
+      applicationRef.bootstrap(componentFactory, div1);
+    })
+    .catch(err => console.log(err));
+
+  // // 应用2的初始化
+  // const div2 = document.createElement('div');
+  // document.body.appendChild(div2);
+  // platform.bootstrapModule(AppModule)
+  //   .then((moduleRef: NgModuleRef<AppModule>) => {
+  //     const applicationRef = moduleRef.injector.get(ApplicationRef);
+  //     const componentFactory: ComponentFactory<AppComponent> = moduleRef.componentFactoryResolver.resolveComponentFactory(AppComponent);
+  //     applicationRef.bootstrap(componentFactory, div2);
+  //   })
+  //   .catch(err => console.log(err));
+});
+button2.addEventListener('click', () => {
+  platform.destroy();
+  // 这里可以更加细粒度控制应用1,2销毁
+});
+
