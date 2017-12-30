cdccba68b7c6bd633c17f13c1aff1d29d72d64d9
HWliao <499606788@qq.com>
2017-12-31 00:22:52 +0800

    最终方案 确定

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {
-  ApplicationRef, ComponentFactory, enableProdMode, NgModuleRef, PlatformRef
+  ApplicationRef, ComponentFactory, ComponentRef, enableProdMode, NgModuleRef
 } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -10,44 +10,31 @@ import { AppComponent } from './app/app.component';
 if (environment.production) {
   enableProdMode();
 }
-let platform: PlatformRef;
-
 const button = document.getElementById('test');
 const button2 = document.getElementById('test2');
 
+let componetRef: ComponentRef<AppComponent>;
 
 button.addEventListener('click', () => {
-  platform = platformBrowserDynamic();
-
   // 应用1 初始化
   const div1 = document.createElement('div');
   document.body.appendChild(div1);
-  platform.bootstrapModule(AppModule)
+  platformBrowserDynamic().bootstrapModule(AppModule)
     .then((moduleRef: NgModuleRef<AppModule>) => {
       // 在这里达成一个platform,多个application启动控制
+      // 多个应用的启动,打包大小有问题
 
       // 获取应用应用
       const applicationRef = moduleRef.injector.get(ApplicationRef);
       // 获取入口组件
       const componentFactory: ComponentFactory<AppComponent> = moduleRef.componentFactoryResolver.resolveComponentFactory(AppComponent);
       // 引导整个组件树的初始化
-      applicationRef.bootstrap(componentFactory, div1);
+      componetRef = applicationRef.bootstrap(componentFactory, div1);
     })
     .catch(err => console.log(err));
-
-  // // 应用2的初始化
-  // const div2 = document.createElement('div');
-  // document.body.appendChild(div2);
-  // platform.bootstrapModule(AppModule)
-  //   .then((moduleRef: NgModuleRef<AppModule>) => {
-  //     const applicationRef = moduleRef.injector.get(ApplicationRef);
-  //     const componentFactory: ComponentFactory<AppComponent> = moduleRef.componentFactoryResolver.resolveComponentFactory(AppComponent);
-  //     applicationRef.bootstrap(componentFactory, div2);
-  //   })
-  //   .catch(err => console.log(err));
 });
 button2.addEventListener('click', () => {
-  platform.destroy();
+  componetRef.destroy();
   // 这里可以更加细粒度控制应用1,2销毁
 });
 
