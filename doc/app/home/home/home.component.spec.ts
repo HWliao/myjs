c7bd97a53ac174a0fb4870d7a7768026d1f55b00
HWliao <499606788@qq.com>
2018-01-29 17:18:43 +0800

    homepage done

--- doc/app/home/home/home.component.spec.ts
+++ doc/app/home/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [HomeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +22,23 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should has h1 title WEB IM PLUGIN', () => {
+    const h1De = fixture.debugElement.query(By.css('header h1'));
+    expect(h1De).toBeTruthy();
+    expect(h1De.nativeElement.innerText).toBe('WEB IM PLUGIN');
+  });
+
+  it('should has h2 secendar title', () => {
+    const h2De = fixture.debugElement.query(By.css('header h2'));
+    expect(h2De).toBeTruthy();
+    expect(h2De.nativeElement.innerText).toBe('用于为系统内部网站提供及时聊天功能');
+  });
+
+  it('should has a image in section', () => {
+    const img = fixture.debugElement.query(By.css('section div img'));
+    expect(img).toBeTruthy();
+    expect(img.nativeElement.src.endsWith('/assets/images/im.png')).toBeTruthy();
   });
 });
