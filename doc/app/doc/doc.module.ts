4d01c290f8185934cca874e032573e7d68eb27c8
HWliao <499606788@qq.com>
2018-01-31 22:34:00 +0800

    code highlight done

--- doc/app/doc/doc.module.ts
+++ doc/app/doc/doc.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { DocComponent } from './doc/doc.component';
 import { DocRoutingModule } from './doc-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { PrismModule } from '@ngx-prism/core';
 
 @NgModule({
   imports: [
     SharedModule,
-    DocRoutingModule
+    DocRoutingModule,
+    PrismModule
   ],
   declarations: [
     DocComponent
--- doc/app/doc/doc/doc.component.ts
+++ doc/app/doc/doc/doc.component.ts
@@ -14,7 +14,7 @@ export class DocComponent implements OnInit {
   constructor() {
     this.currItem = new DocListItemModel(0, 'trending_up', '快速开始', '入门', true);
     this.currItem.desc = `
-    /**
+/**
  * 列表项目
  * @author lhw
  */
@@ -54,9 +54,7 @@ export class DocListItemModel {
    *
    */
   desc: string;
-}
-
-    `;
+}`;
     this.items = [];
     this.items.push(new DocListItemModel(0, 'trending_up', '快速开始', '入门', true));
     this.items.push(new DocListItemModel(1, 'settings', '详细配置', '配置', true));
@@ -76,6 +74,18 @@ export class DocListItemModel {
     this.items.push(new DocListItemModel(15, 'label', '登入'));
     this.items.push(new DocListItemModel(16, 'label', '登入'));
     this.items.push(new DocListItemModel(17, 'label', '登入'));
+    this.items[0].desc = `
+    this.items.push(new DocListItemModel(7, 'label', '登入'));
+    this.items.push(new DocListItemModel(8, 'label', '登入'));
+    this.items.push(new DocListItemModel(9, 'label', '登入'));
+    this.items.push(new DocListItemModel(10, 'label', '登入'));
+    this.items.push(new DocListItemModel(11, 'label', '登入'));
+    this.items.push(new DocListItemModel(12, 'label', '登入'));
+    this.items.push(new DocListItemModel(13, 'label', '登入'));
+    this.items.push(new DocListItemModel(14, 'label', '登入'));
+    this.items.push(new DocListItemModel(15, 'label', '登入'));
+    this.items.push(new DocListItemModel(16, 'label', '登入'));
+    this.items.push(new DocListItemModel(17, 'label', '登入'));`;
   }
 
   ngOnInit() {
