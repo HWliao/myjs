3df8356ed7fa9a5f7975ebda61b69b28c50c70b6
HWliao <499606788@qq.com>
2018-02-02 21:48:23 +0800

    动态生成 mat menu

--- doc/app/main/operation-panel/operation-panel.component.ts
+++ doc/app/main/operation-panel/operation-panel.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, TemplateRef, ViewChildren } from '@angular/core';
+import { MatMenu, MatMenuPanel, MenuPositionX, MenuPositionY } from '@angular/material';
 
 @Component({
   selector: 'im-doc-operation-panel',
   templateUrl: './operation-panel.component.html',
   styleUrls: ['./operation-panel.component.css']
 })
-export class OperationPanelComponent implements OnInit {
+export class OperationPanelComponent implements OnInit, AfterViewInit {
+  private rootMenu: MatMenu;
 
-  constructor() { }
+  @ViewChildren(MatMenu) menus: QueryList<MatMenu>;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    console.log(this.menus);
+    setTimeout(() => {
+      if (this.menus) {
+        this.rootMenu = this.menus.first;
+      }
+    }, 0);
+
+  }
 }
