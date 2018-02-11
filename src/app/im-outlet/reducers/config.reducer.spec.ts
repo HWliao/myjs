44cbc57b2db353fc2d66c12d6662c98da8a3360c
HWliao <499606788@qq.com>
2018-02-11 14:37:38 +0800

    test: config reducer test done

--- src/app/im-outlet/reducers/config.reducer.spec.ts
+++ src/app/im-outlet/reducers/config.reducer.spec.ts
@@ -1,3 +1,22 @@
-describe('Config Reducer', () => {
+import { initialState, reducer } from './config.reducer';
+import { ConfigSetAction } from '../actions/config.actions';
 
+describe('Config Reducer', () => {
+  describe('unknow action', () => {
+    it('should return the initial state', () => {
+      const action = {} as any;
+      const result = reducer(initialState, action);
+      expect(result).toBe(initialState);
+    });
+  });
+
+  describe('config set action', () => {
+    it('should override the result state', () => {
+      const action = new ConfigSetAction({ appKey: 'test' });
+      const result = reducer(initialState, action);
+      Object.keys(action.payload).forEach((key) => {
+        expect(result[key]).toBe(action.payload[key]);
+      });
+    });
+  });
 });
