c7dcd2e29284c81c50d8abff21ba67393b80d98c
HWliao <499606788@qq.com>
2018-02-12 16:38:30 +0800

    test(config): 完善setConfig和getConfig 单元测试

--- src/app/im-outlet/service/outlet.service.spec.ts
+++ src/app/im-outlet/service/outlet.service.spec.ts
@@ -7,10 +7,13 @@ import { LoggerService } from '../../core/logger/logger.service';
 import * as AppState from '../../reducers';
 import * as OutletState from '../reducers';
 import { AppDestroyAction, AppInitAction } from '../../actions/app.actions';
+import { ConfigModel } from '../models/config.model';
+import { ConfigSetAction } from '../actions/config.actions';
 
 describe('OutletService', () => {
 
   let store: Store<any>;
+  let service: OutletService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,24 +27,47 @@ describe('OutletService', () => {
     });
     store = TestBed.get(Store);
     spyOn(store, 'dispatch');
+    service = TestBed.get(OutletService);
   });
 
-  it('should be created', inject([OutletService], (service: OutletService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 
-  it('should dispatch APP_INIT action', inject([OutletService], (service: OutletService) => {
-    service.setInit(true);
-    expect(store.dispatch).toHaveBeenCalledWith(new AppInitAction());
-  }));
+  describe('setInit', () => {
+    it('should dispatch APP_INIT action', () => {
+      service.setInit(true);
+      expect(store.dispatch).toHaveBeenCalledWith(new AppInitAction());
+    });
 
-  it('should dispatch APP_DESTROY action', inject([OutletService], (service: OutletService) => {
-    service.setInit(false);
-    expect(store.dispatch).toHaveBeenCalledWith(new AppDestroyAction());
-  }));
+    it('should dispatch APP_DESTROY action', () => {
+      service.setInit(false);
+      expect(store.dispatch).toHaveBeenCalledWith(new AppDestroyAction());
+    });
+  });
 
-  it('should inited be true', inject([OutletService], (service: OutletService) => {
-    store.next(new AppInitAction());
-    expect(service.isInited()).toBeTruthy();
-  }));
+  describe('isInited', () => {
+    it('should inited be true', () => {
+      store.next(new AppInitAction());
+      expect(service.isInited()).toBeTruthy();
+    });
+  });
+
+
+  fdescribe('config', () => {
+    it('should dispatch CONFIG_SET_ACTION', () => {
+      const config: ConfigModel = { appKey: 'test' };
+      service.setConfig(config);
+      expect(store.dispatch).toHaveBeenCalledWith(new ConfigSetAction(config));
+    });
+
+    it('should get the config', () => {
+      const config: ConfigModel = { appKey: 'test' };
+      store.next(new ConfigSetAction(config));
+      const theConfig = service.getConfig();
+      Object.keys(config).forEach((key) => {
+        expect(theConfig[key]).toBe(config[key]);
+      });
+    });
+  });
 });
