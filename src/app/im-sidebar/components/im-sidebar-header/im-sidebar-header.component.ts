5b94e2e38f9a2799521316f2c33fe02a2206ce9e
HWliao <499606788@qq.com>
2018-02-25 22:41:50 +0800

    feat(UI): sidebar header 20%

--- src/app/im-sidebar/components/im-sidebar-header/im-sidebar-header.component.ts
+++ src/app/im-sidebar/components/im-sidebar-header/im-sidebar-header.component.ts
@@ -1,10 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'im-sidebar-header',
   templateUrl: './im-sidebar-header.component.html'
 })
 export class ImSidebarHeaderComponent implements OnInit {
+
+  /**
+   * 收起按钮的标题
+   * @type {string}
+   */
+  @Input() title = '';
+
+  /**
+   * 总未读数
+   * @type {number}
+   */
+  @Input() unread = 0;
+
+  /**
+   * 是否闪动
+   * @type {boolean}
+   */
+  @Input() shake = false;
 
   constructor() {
   }
