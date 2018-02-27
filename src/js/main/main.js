0abf52d1b665e729c150dfb80d1d23bb68e9f841
HWliao <499606788@qq.com>
2018-02-27 14:37:39 +0800

    chore(im-docs): 清理掉react demo

--- src/js/main/main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-export function start() {
-  const div = document.createElement('div');
-  document.body.appendChild(div);
-  ReactDOM.render(<App/>, div);
-}
