448132e0a98da10b1b80ff07964b1138e219391b
HWliao <499606788@qq.com>
2018-02-28 23:19:58 +0800

    chore(im-docs): 更换为material-ui

--- src/docs/ImDocApp.js
+++ src/docs/ImDocApp.js
@@ -1,13 +1,15 @@
-import './theme.scss';
 import React from 'react';
-import Button from 'rmwc/Button';
+import Button from 'material-ui/Button';
+import Reboot from 'material-ui/Reboot';
 
 export default class ImDocApp extends React.Component {
   render() {
     return (
       <div>
-        <span>Hello wold!</span>
-        <Button unelevated>廖红卫</Button>
+        <Reboot/>
+        <Button variant="raised" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
