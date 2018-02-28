10085d269c9ad8b51e5ec3db0a336c27766fcb64
HWliao <499606788@qq.com>
2018-02-28 17:27:49 +0800

    chore(im-docs): create-react-app 必须eject

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Button from 'rmwc/Button';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button/>
       </div>
     );
   }
