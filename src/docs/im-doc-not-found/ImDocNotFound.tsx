4c6b6ffcb492c287fadfbd4b7644f001ce968744
HWliao <499606788@qq.com>
2018-03-03 18:12:18 +0800

    docs: 404页面 done

--- src/docs/im-doc-not-found/ImDocNotFound.tsx
+++ src/docs/im-doc-not-found/ImDocNotFound.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
 import { RouteProps } from 'react-router-dom';
+import { StyleRulesCallback, withStyles, WithStyles } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
 
-export default class ImDocNotFound extends React.Component<RouteProps> {
+type StyleType = 'root';
+type Props = RouteProps & WithStyles<StyleType>;
+
+const styles: StyleRulesCallback<StyleType> = theme => ({
+  root: {
+    color: theme.palette.secondary.main,
+    flex: '1',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+class ImDocNotFound extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
 
   render() {
+    const { classes } = this.props;
     return (
-      <span>not found</span>
+      <Typography variant="display3" className={classes.root}>404</Typography>
     );
   }
 }
+
+export default withStyles(styles)<{}>(ImDocNotFound);
