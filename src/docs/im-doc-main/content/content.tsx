2ecba52ca8b8a285abc095045b080f2ea55103b7
HWliao <499606788@qq.com>
2018-03-04 11:09:49 +0800

    docs: content done

--- src/docs/im-doc-main/content/content.tsx
+++ src/docs/im-doc-main/content/content.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import { StyleRulesCallback, WithStyles, withStyles } from 'material-ui/styles';
+import SyntaxHighlighter from 'react-syntax-highlighter/prism';
+import { atomDark } from 'react-syntax-highlighter/styles/prism';
 
-type StyleType = 'root';
+type StyleType = 'root' | 'header' | 'section';
 
 type Props = WithStyles<StyleType>;
 
 const styles: StyleRulesCallback<StyleType> = theme => ({
   root: {
-    padding: 20,
-    flex: '1',
-    display: 'flex'
+    flex: '1'
+  },
+  header: {
+    background: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    paddingLeft: 20,
+    display: 'flex',
+    alignItems: 'center',
+    '& h1': {
+      fontWeight: 300,
+      margin: 0,
+      padding: '28px 8px',
+      fontSize: 20,
+      color: '#ffffff'
+    }
+  },
+  section: {
+    padding: '20px 70px 50px'
   }
 });
 
@@ -22,7 +39,14 @@ class ImDocContent extends React.Component<Props> {
     const {classes} = this.props;
     return (
       <div className={classes.root}>
-        content
+        <header className={classes.header}>
+          <h1>{'header'}</h1>
+        </header>
+        <section className={classes.section}>
+          <SyntaxHighlighter language={'javascript'} style={atomDark}>
+            {this.props.children}
+          </SyntaxHighlighter>
+        </section>
       </div>
     );
   }
--- src/docs/im-doc-main/content/quick-stated/QuickStarted.tsx
+++ src/docs/im-doc-main/content/quick-stated/QuickStarted.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
+import Content from '../content';
 
 export const QUICK_STARTED_TO = '/doc/quick_started';
 
-class QuickStarted extends React.Component {
-  constructor(props: {}) {
-    super(props);
-  }
+const content = `
+  // 你说是吗
+  var x = 1;
+  console.log(1111);
+`;
 
+class QuickStarted extends React.Component<{}, {}> {
   render() {
     return (
-      <div>QuickStarted</div>
+      <Content>
+        {content}
+      </Content>
     );
   }
 }
