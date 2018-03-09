4f6f699fca64918fb2b5d3e2833c459a797c58e2
HWliao <499606788@qq.com>
2018-03-09 15:06:33 +0800

    test(im-root): mount/unmount unit test

--- src/main/container/im-root.test.tsx
+++ src/main/container/im-root.test.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import '../rxjs';
-import ImRoot from './im-root';
+import ImRoot, { mount, unmount } from './im-root';
 
 describe('im root component', () => {
   describe('mount', () => {
     it('should not crash', () => {
       const div = document.createElement('div');
       ReactDOM.render(<ImRoot/>, div);
+    });
+  });
+
+  describe('mount/unmout', () => {
+    let div: HTMLElement;
+
+    beforeEach(() => {
+      div = document.createElement('div');
+      document.body.appendChild(div);
+    });
+
+    it('should not crash mount/unmount', () => {
+      return mount(div).then(() => {
+        return unmount(div);
+      });
     });
   });
 });
