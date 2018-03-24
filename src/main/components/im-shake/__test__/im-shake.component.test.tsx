b61b0367ba33325db73caa3781cd3c5886773a76
HWliao <499606788@qq.com>
2018-03-24 14:31:09 +0800

    test: shake component test done

--- src/main/components/im-shake/__test__/im-shake.component.test.tsx
+++ src/main/components/im-shake/__test__/im-shake.component.test.tsx
@@ -4,10 +4,12 @@ import ImShake, { Props, State } from '../im-shake.component';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
+import Spy = jasmine.Spy;
 
 describe('im shake component', () => {
   let wrapper: ShallowWrapper<Props, State>;
   let timer$: Subject<boolean>;
+  let intervalSpy: Spy;
   beforeEach(() => {
     wrapper = shallow(
       <ImShake
@@ -17,10 +19,29 @@ describe('im shake component', () => {
     );
 
     timer$ = new BehaviorSubject(true);
-    spyOn(Observable, 'interval').and.returnValue(timer$);
+    intervalSpy = spyOn(Observable, 'interval').and.returnValue(timer$);
   });
   it('should be init', () => {
-    const instance = wrapper.instance() as ImShake;
-    console.log(instance.getSubscription());
+    expect(wrapper.type()).toEqual('div');
+  });
+  describe('set shake to true', () => {
+    it('should toggle the div"s style visibility with interval', () => {
+      wrapper.setProps({shake: true});
+      expect(intervalSpy).lastCalledWith(100);
+      const v1 = wrapper.state('visible');
+      expect(wrapper.prop('style')).toHaveProperty('visibility', v1 ? 'visible' : 'hidden');
+      timer$.next();
+      wrapper.update();
+      expect(wrapper.state('visible')).toEqual(!v1);
+      expect(wrapper.prop('style')).toHaveProperty('visibility', !v1 ? 'visible' : 'hidden');
+    });
+  });
+  describe('unmount component', () => {
+    it('should called unsubscribe', () => {
+      const instance = wrapper.instance() as ImShake;
+      const spy = spyOn(instance.getSubscription(), 'unsubscribe');
+      wrapper.unmount();
+      expect(spy).toBeCalled();
+    });
   });
 });
